4056e261beae120e74e4794a79c7a6ccfb37e9ed
bryan mcwatters <bmcwatters@comcast.net>
2021-12-09 10:03:56 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using ClassesExercise;
+
+Car car = new Car();
+car.Make = "Hyundai";
+car.Model = "Sonata";
+car.Year = 2021;
+
+var carList = new List<Car>();
+carList.Add(car);
+
+foreach(var vehicle in carList)
+{
+    Console.WriteLine($"{vehicle.Model} {vehicle.Year} {vehicle.Make}");
+}
+
+
+/*
+Car car = new Car()
+{
+Make = "Ford",
+Model = "Taurus",
+Year = 2019
+};
+*/
